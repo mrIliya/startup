ffb78f2c08a19e4669abbb07618fd3ef5fee6a38
mriliya <mr.litvishchenko@gmail.com>
2020-06-21 16:23:13 +0300

    added mobile menu

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,17 @@ $(function () {
 		infinite: true,
 		prevArrow: '<i class="icon-chevron-left slider-arrow__left"></i>',
 		nextArrow: '<i class="icon-chevron-right slider-arrow__right"></i>',
+		responsive: [
+			{
+				breakpoint: 1100,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					variableWidth: true,
+					centerMode: true,
+				}
+			}
+		]
 	});
 
 
@@ -22,12 +33,39 @@ $(function () {
 		return false;
 	});
 
+	$('.works-content .works-content__btn').on('click', function () {
+		let id = $(this).attr('data-id');
+
+		$('.works-content').find('.mobile-slider').removeClass('active').hide();
+		$('.works-content').find('.works-content__btn').removeClass('active');
+
+		$(this).addClass('active');
+
+		$('#' + id).addClass('active').fadeIn();
+		return false;
+	});
+
 
 	$('.review-slider').slick({
 		arrows: false,
 		dots: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		speed: 1000
+		speed: 1500,
+		autoplay: true,
+	});
+
+
+	$('.header__button').on('click', function () {
+		$(this).toggleClass('active');
+		$('.menu').toggleClass('active').slideDown('600');
+	});
+
+	$('.mobile-slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		variableWidth: true,
+		centerMode: true,
+		arrows: false
 	});
 });
